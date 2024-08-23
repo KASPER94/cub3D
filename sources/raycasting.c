fe65966e9f9185de65fe9cca1fc240bbc0584d5c
Simon Kaperski <skapersk@made-f0Br3s8.clusters.42paris.fr>
2024-08-23 10:53:08 +0200

    fix hit wall but problem parsing

--- sources/raycasting.c
+++ sources/raycasting.c
@@ -3,14 +3,13 @@
 /*                                                        :::      ::::::::   */
 /*   raycasting.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cdeville <cdeville@student.42.fr>          +#+  +:+       +#+        */
+/*   By: skapersk <skapersk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 12:33:01 by skapersk          #+#    #+#             */
-/*   Updated: 2024/08/22 12:02:54 by cdeville         ###   ########.fr       */
+/*   Updated: 2024/08/23 10:14:38 by skapersk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include <stdio.h>
 #include "cub3d.h"
 
 void	init_vectors(int x, int y)
@@ -38,8 +37,6 @@ void	find_ray_and_dir(int x, int w)
 
 void	DDA(void)
 {
-	double cell_size = data()->var.perpWallDist / sqrt(pow((data()->var.mapX - data()->var.position_x), 2) + pow((data()->var.mapY - data()->var.position_y), 2));
-	data()->var.test = cell_size;
 	while (data()->var.hit == 0)
 	{
 		if (data()->var.sideDistX < data()->var.sideDistY)
@@ -55,13 +52,11 @@ void	DDA(void)
 			data()->var.side = 1;
 		}
 		if (data()->var.mapY < 0.25 || data()->var.mapX < 0.25
-			|| data()->var.mapY > data()->width - 1.25
-			|| data()->var.mapX > data()->height - 0.25)
-			break ;
-		else if (data()->map[data()->var.mapX][data()->var.mapY] > '0')
-		{
+			|| data()->var.mapY > data()->width - 0.25
+			|| data()->var.mapX > data()->height - 1.25)
 			data()->var.hit = 1;
-		}
+		if (data()->map[data()->var.mapX][data()->var.mapY] == '1')
+			data()->var.hit = 1;
 	}
 }
 
@@ -100,29 +95,6 @@ void	find_lowest_and_high_pix(void)
 	data()->var = tmp;
 }
 
-void	calcul_wall_value(void)
-{
-	if (data()->var.side == 1)
-	{
-		data()->var.wall = (data()->var.stepY == -1) ? 0 : 1;
-	}
-	else
-	{
-		data()->var.wall = (data()->var.stepX == -1) ? 2 : 3;
-	}
-	// if (data()->var.side == 0)
-	// {
-	// 	data()->var.wall = data()->var.position_y
-	// 	+ data()->var.perpWallDist * data()->var.rayDirY;
-	// }
-	// else
-	// {
-	// 	data()->var.wall = data()->var.position_x
-	// 	+ data()->var.perpWallDist * data()->var.rayDirX;
-	// }
-	// data()->var.wall -= floor(data()->var.wall);
-}
-
 void	coor_text(void)
 {
 	if (data()->var.side == 0)
@@ -135,139 +107,7 @@ void	coor_text(void)
 		data()->var.texture_x = TEXTURE_WIDTH - data()->var.texture_x - 1;
 	if (data()->var.side == 1 && data()->var.rayDirY < 0)
 		data()->var.texture_x = TEXTURE_WIDTH - data()->var.texture_x - 1;
-
-	// int	tex;
-
-	// tex = (int)(data()->var.wall * (double)TEXTURE_WIDTH);
-	// if (data()->var.side == 0)
-	// {
-	// 	tex = TEXTURE_WIDTH - tex - 1;
-	// }
-	// if (data()->var.side == 1)
-	// {
-	// 	tex = TEXTURE_WIDTH - tex - 1;
-	// }
-	// data()->var.texture_x = tex;
 }
-
-
-// void	coor_text(void)
-// {
-// 	int	tex;
-
-// 	tex =(int)(data()->var.wall * (double)TEXTURE_WIDTH);
-// 	if (data()->var.side == 0 && data()->var.rayDirX > 0)
-// 		tex = TEXTURE_WIDTH - data()->var.texture_x - 1;
-// 	if (data()->var.side == 1 && data()->var.rayDirY < 0)
-// 		tex = TEXTURE_WIDTH - data()->var.texture_x - 1;
-// 	data()->var.texture_x = tex;
-// }
-
-// void draw_vertical_texture_stripe(int x)
-// {
-//     double step;
-//     double textures_position;
-//     int y;
-
-
-// 	calcul_wall_value();
-//     // Calcul du step pour l'incrémentation de la position dans la texture
-//     step = 1.0 * TEXTURE_HEIGHT / data()->var.lineHeight;
-//     textures_position = (data()->var.drawStart - HEIGHT / 2 + data()->var.lineHeight / 2) * step;
-//     my_floor(x);
-//     my_cell(x);
-//     y = data()->var.drawStart;
-//     while (y < data()->var.drawEnd) {
-//         // Calcul de la coordonnée y de la texture
-//         data()->var.texture_y = (int)textures_position & (TEXTURE_HEIGHT - 1);
-//         textures_position += step;
-
-//         // Placement du pixel avec la couleur de la texture
-//         int color = (*(int *)(data()->img2[data()->var.wall].addr + (4 * data()->img2[1].width * data()->var.texture_y) + 4 * data()->var.texture_x));
-//         if (data()->var.side == 1) {
-//             color = (color >> 1) & 8355711; // Appliquer une ombre pour les côtés
-//         }
-//         mlx_place_pixel(x, y, color);
-//         y++;
-//     }
-// }
-
-
-// void	draw_vertical_texture_stripe(int x)
-// {
-// 	double	step;
-// 	double	textures_position;
-// 	int		y;
-
-// 	int	wall;
-
-
-// 	if (data()->var.side == 1)
-// 	{
-// 		wall = (data()->var.stepY == -1) ? 0 : 1;
-// 	}
-// 	else
-// 	{
-// 		wall = (data()->var.stepX == -1) ? 2 : 3;
-// 	}
-// 	y = data()->var.drawStart;
-// 	step = 1.0 * TEXTURE_HEIGHT / data()->var.lineHeight;
-// 	textures_position = (data()->var.drawStart - HEIGHT / 2 + data()->var.lineHeight / 2) * step;
-// 	my_floor(x);
-// 	my_cell(x);4 * data()->img2[1].width * y_tex
-// 	while (y < data()->var.drawEnd)
-// 	{
-// 		data()->var.texture_y = (int)textures_position & (TEXTURE_HEIGHT - 1);
-// 		mlx_place_pixel(x, y, data()->textures[wall][TEXTURE_HEIGHT * data()->var.texture_y + data()->var.texture_x]);
-// 		y++;
-// 	}
-
-/*
-
-	step = 1.0 * TEXTURE_HEIGHT / data()->var.lineHeight;
-	textures_position = (data()->var.drawStart - HEIGHT / 2 + data()->var.lineHeight / 2) * step;
-	y = data()->var.drawStart;
-	while (y < data()->var.drawEnd)
-	{
-		data()->var.texture_y = (int)textures_position & (TEXTURE_HEIGHT - 1);
-		textures_position += step;
-		data()->var.color = data()->textures[data()->var.texture_number][TEXTURE_HEIGHT * data()->var.texture_y + data()->var.texture_x];
-		if (data()->var.side == 1)
-			data()->var.color = (data()->var.color >> 1) & 8355711;
-		mlx_place_pixel(x, y, data()->var.color);
-		y++;
-	}*/
-// }
-
-
-
-
-
-// void temp(int x) {
-// 	int y = 0;
-
-// 	t_img	tex;
-//     void *addr;
-// 	tex = data()->img2[1];
-// 	addr = tex.addr;
-// 	int x_tex;
-// 	int y_tex;
-// 	int clr;
-// 	my_floor(x);
-//     my_cell(x);
-//     y = data()->var.drawStart;
-// 	printf("%d\n", data()->var.mapX);
-//     while (y < data()->var.drawEnd) {
-// 		x_tex = (double)x / 1900.0 * tex.width;
-// 		y_tex = (double)y / data()->var.drawEnd * tex.height;
-// 		// printf("%f\n", x_tex);
-// 		// printf("%f\n\n", y / 200.0);
-// 		clr = (*(int *)(addr + (4 * tex.width * y_tex) + 4 * x_tex));
-
-// 		mlx_place_pixel(x, y, clr);
-// 		y++;
-// 	}
-// }
 
 void temp(int x)
 {
@@ -303,7 +143,6 @@ void temp(int x)
 	}
 }
 
-
 int	raycast_loop(void)
 {
 	int	x;
@@ -320,239 +159,8 @@ int	raycast_loop(void)
 		set_height_wall();
 		find_lowest_and_high_pix();
 		coor_text();
-		// draw_vertical_texture_stripe(x);
-		// draw_wall(x);
 		temp(x);
 		x++;
 	}
 	return (0);
 }
-
-
-// #include "cub3d.h"
-
-// void	set_values(double direction_x, double direction_y, double pla_x,
-// 		double pla_y)
-// {
-// 	data()->var.dirX = direction_x;
-// 	data()->var.dirY = direction_y;
-// 	data()->var.planeX = pla_x;
-// 	data()->var.planeY = pla_y;
-// }
-
-// void	init_vectors(int x, int y)
-// {
-// 	data()->var.position_x = x + 0.5;
-// 	data()->var.position_y = y + 0.5;
-// 	if (data()->map[x][y] == 'N')
-// 		set_values(0, -1, 0.66, 0);
-// 	else if (data()->map[x][y] == 'S')
-// 		set_values(0, 1, -0.66, 0);
-// 	else if (data()->map[x][y] == 'E')
-// 		set_values(1, 0, 0, 0.66);
-// 	else if (data()->map[x][y] == 'W')
-// 		set_values(-1, 0, 0, -0.66);
-// }
-
-// void	find_ray_and_dir(int x, int w)
-// {
-// 	data()->var.camera = 2 * x / (double)w - 1;
-// 	data()->var.rayDirX = data()->var.dirX + data()->var.planeX * data()->var.camera;
-// 	data()->var.rayDirY = data()->var.dirY + data()->var.planeY * data()->var.camera;
-// }
-
-// void	set_box(void)
-// {
-// 	t_var	tmp;
-
-// 	tmp = data()->var;
-// 	tmp.mapX = (int)tmp.position_x;
-// 	tmp.mapY = (int)tmp.position_y;
-// 	tmp.hit = 0;
-// 	data()->var = tmp;
-// }
-
-// void set_len_ray(void)
-// {
-// 	t_var	tmp;
-
-// 	tmp = data()->var;
-// 	if (tmp.rayDirX == 0)
-// 		tmp.deltaDistX = 1e30;
-// 	else
-// 		tmp.deltaDistX = fabs(1 / tmp.rayDirX);
-// 	if (tmp.rayDirY == 0)
-// 		tmp.deltaDistY = 1e30;
-// 	else
-// 		tmp.deltaDistY = fabs(1 / tmp.rayDirY);
-// 	data()->var = tmp;
-// }
-
-// void	set_step_and_ini_sideDist(void)
-// {
-// 	t_var	tmp;
-
-// 	tmp = data()->var;
-// 	if (tmp.rayDirX < 0)
-//       {
-//         tmp.stepX = -1;
-//         tmp.sideDistX = (tmp.position_x - tmp.mapX) * tmp.deltaDistX;
-//       }
-//       else
-//       {
-//         tmp.stepX = 1;
-//         tmp.sideDistX = (tmp.mapX + 1.0 - tmp.position_x) * tmp.deltaDistX;
-//       }
-//       if (tmp.rayDirY < 0)
-//       {
-//         tmp.stepY = -1;
-//         tmp.sideDistY = (tmp.position_y - tmp.mapY) * tmp.deltaDistY;
-//       }
-//       else
-//       {
-//         tmp.stepY = 1;
-//         tmp.sideDistY = (tmp.mapY + 1.0 - tmp.position_y) * tmp.deltaDistY;
-//       }
-// 	data()->var = tmp;
-// }
-
-// void	DDA(void)
-// {
-// 	t_var	tmp;
-
-// 	tmp = data()->var;
-// 	while (tmp.hit == 0)
-// 	{
-// 		if (tmp.sideDistX < tmp.sideDistY)
-// 		{
-// 			tmp.sideDistX += tmp.deltaDistX;
-// 			tmp.mapX += tmp.stepX;
-// 			tmp.side = 0;
-// 		}
-// 		else
-// 		{
-// 			tmp.sideDistY += tmp.deltaDistY;
-// 			tmp.mapY += tmp.stepY;
-// 			tmp.side = 1;
-// 		}
-// 		if (data()->map[tmp.mapX][tmp.mapY] > 0)
-// 			tmp.hit = 1;
-// 	}
-// 	data()->var = tmp;
-// }
-
-// void	dist_project_camera(void)
-// {
-// 	t_var	tmp;
-
-// 	tmp = data()->var;
-// 	if (tmp.side == 0)
-// 		tmp.perpWallDist = (tmp.sideDistX - tmp.deltaDistX);
-// 	else
-// 		tmp.perpWallDist = (tmp.sideDistY - tmp.deltaDistY);
-// 	data()->var = tmp;
-// }
-
-// void	set_height_wall(void)
-// {
-// 	t_var	tmp;
-
-// 	tmp = data()->var;
-// 	tmp.lineHeight = (int)(HEIGHT / tmp.perpWallDist);
-// 	data()->var = tmp;
-// 	// ft_printf("%d ..... ", data()->var.lineHeight);
-// }
-
-// void	find_lowest_and_high_pix(void)
-// {
-// 	t_var	tmp;
-
-// 	tmp = data()->var;
-// 	tmp.drawStart = -tmp.lineHeight / 2 + HEIGHT / 2;
-// 	if (tmp.drawStart < 0)
-// 		tmp.drawStart = 0;
-// 	tmp.drawEnd = tmp.lineHeight / 2 + HEIGHT / 2;
-// 	if (tmp.drawEnd >= HEIGHT)
-// 		tmp.drawEnd = HEIGHT - 1;
-// 	data()->var = tmp;
-// 	// ft_printf("%d ..... ", tmp.drawStart);
-// 	// ft_printf("%d\n", tmp.lineHeight);
-// }
-
-// void	set_color(void)
-// {
-// 	data()->color = malloc(sizeof(t_test_color));
-// 	data()->color.blue.r = 80;
-// 	data()->color.blue.g = 91;
-// 	data()->color.blue.b = 166;
-// 	data()->color.red.r = 175;
-// 	data()->color.red.g = 54;
-// 	data()->color.red.b = 60;
-// 	data()->color.green.r = 70;
-// 	data()->color.green.g = 148;
-// 	data()->color.green.b = 73;
-// 	    // Ceiling color
-//     data()->color.white.r = 255;
-//     data()->color.white.g = 255;
-//     data()->color.white.b = 255;
-
-//     // Wall color
-//     data()->color.yellow.r = 255;
-//     data()->color.yellow.g = 223;
-//     data()->color.yellow.b = 0;
-// }
-
-// void	mlx_place_pixel(int x, int y, int colour)
-// {
-// 	char	*distance;
-
-// 	distance = data()->img.addr + (y * data()->img.line_len + x * (data()->img.bpp / 8));
-// 	*(unsigned int *)distance = colour;
-// }
-
-// void	draw_wall(int x)
-// {
-// 	int	y;
-// 	int	color;
-
-// 	if (data()->var.side == 1)
-// 	{
-//         color = (data()->var.stepY == -1) ? (data()->color.red.r << 16 | data()->color.red.g << 8 | data()->color.red.b)
-//                                   : (data()->color.green.r << 16 | data()->color.green.g << 8 | data()->color.green.b);
-// 	}
-// 	else
-// 	{
-//         color = (data()->var.stepX == -1) ? (data()->color.blue.r << 16 | data()->color.blue.g << 8 | data()->color.blue.b)
-//                                   : (data()->color.green.r << 16 | data()->color.green.g << 8 | data()->color.green.b);
-// 	}
-// 	y = data()->var.drawStart;
-// 	// ft_printf("%d --- %d\n", y, data()->var.drawEnd);
-// 	while (y < data()->var.drawEnd)
-// 	{
-// 		mlx_place_pixel(x, y, color);
-// 		y++;
-// 	}
-// }
-
-// int	raycast_loop(void)
-// {
-// 	int	x;
-// 	int	i;
-
-// 	x = 0;
-// 	i = 0;
-// 	while (x < WIDTH)
-// 	{
-// 		find_ray_and_dir(x, WIDTH);
-// 		set_box();
-// 		set_len_ray();
-// 		set_step_and_ini_sideDist();
-// 		DDA();
-// 		dist_project_camera();
-// 		set_height_wall();
-// 		find_lowest_and_high_pix();
-// 		draw_wall(x);
-// 		x++;
-// 	}
-// 	return (i);
-// }
--- sources/textures.c
+++ sources/textures.c
@@ -3,33 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   textures.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cdeville <cdeville@student.42.fr>          +#+  +:+       +#+        */
+/*   By: skapersk <skapersk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/21 21:41:46 by skapersk          #+#    #+#             */
-/*   Updated: 2024/08/21 17:16:28 by cdeville         ###   ########.fr       */
+/*   Updated: 2024/08/23 09:25:48 by skapersk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
-
-// void fill_img2_with_textures(int i)
-// {
-//     int x;
-//     int y;
-//     int *addr;
-
-//     addr = (int *)(data()->img2[i].addr);
-//     x = 0;
-//     while (x < data()->img2[i].height) {
-//         y = 0;
-//         while (y < data()->img2[i].width) {
-//             data()->textures[i][data()->img2[i].height * x + y] = addr[data()->img2[i].height * x + y];
-//             y++;
-//         }
-//         x++;
-//     }
-// }
-
 
 void	fill_img2_with_textures(int i)
 {
@@ -74,50 +55,8 @@ int	init_textures(void)
 		if (!addr)
 			return (1);
 		data()->img2[i].addr = addr;
-		// fill_img2_with_textures(i);
-		// mlx_destroy_image(data()->mlx, data()->img2[i].pointer_to_img);
 		current = current->next;
 		i++;
 	}
 	return (0);
 }
-
-// void fill_img2_with_textures(void)
-// {
-//     int x;
-//     int y;
-//     int *addr;
-
-//     addr = (int *)(data()->txt.addr);
-//     x = 0;
-//     while (x < data()->txt.height) {
-//         y = 0;
-//         while (y < data()->txt.width) {
-//             data()->textures[0][data()->txt.height * x + y] = addr[data()->txt.height * x + y];
-//             y++;
-//         }
-//         x++;
-//     }
-// }
-
-// int	init_textures(void)
-// {
-// 	void	*tmp;
-// 	void	*addr;
-
-// 	tmp = mlx_xpm_file_to_image(data()->mlx, data()->xpm->val,
-// 			&(data()->txt.width), &(data()->txt.height));
-// 	// if (!tmp)
-// 	// 	return (1);
-// 	data()->txt.pointer_to_img = tmp;
-// 	addr = (int *)mlx_get_data_addr(data()->txt.pointer_to_img,
-// 			&data()->txt.bpp, &data()->txt.line_len,
-// 			&data()->txt.endian);
-// 	// if (!addr)
-// 	// 	return (1);
-// 	data()->txt.addr = addr;
-// 	fill_img2_with_textures();
-// 	// mlx_destroy_image(data()->mlx, data()->txt.pointer_to_img);
-// 	data()->xpm = data()->xpm->next;
-// 	return (0);
-// }
